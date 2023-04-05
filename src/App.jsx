f8b70cd414bd15d36d60e1e4523d82d2dfc0a911
Narmin Aliyeva <narmingale@gmail.com>
2023-04-06 02:47:23 +0400

    add: project dependencies

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.scss";
+import { Button } from "react-bootstrap";
+
+export default function App() {
+  return (
+    <div className="App">
+      header
+      <Button variant="primary" className="butto-click">
+        Click me
+      </Button>
+      ;
+    </div>
+  );
+}
---
+++ src/utils/constants.js
@@ -0,0 +1,5 @@
+export const API_BASE_URL = "https://www.themealdb.com/api/json/v1/1";
+export const MEALS_BY_CAT_NAME =
+  "https://www.themealdb.com/api/json/v1/1/filter.php?c=";
+export const API_CAT_LIST =
+  "https://www.themealdb.com/api/json/v1/1/categories.php";
